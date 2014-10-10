df2c0776f9f8f6c5dfd0cf8855e71d954bc62dc5
Julio Canares <juliocanares@gmail.com>
2014-10-10 00:12:05 -0700

    setup express basic static path

--- server.js
+++ server.js
@@ -5,3 +5,10 @@
 var express = require('express');
 
 var app = express();
+
+/**
+ * Setup express static path
+ * ====================================================
+ */
+app.use(express.static('./public'));
+
